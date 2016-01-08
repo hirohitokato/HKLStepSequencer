abcc9838453767afe61d84debc21317b740f1b86
Hirohito Kato <hkato193@gmail.com>
2016-01-08 23:04:13 +0900

    Initial Commit

---
+++ KSLSynthesizer/KSLSynthesizer.h
@@ -0,0 +1,19 @@
+//
+//  KSLSynthesizer.h
+//  KSLSynthesizer
+//
+//  Created by 加藤寛人 on 2016/01/08.
+//  Copyright © 2016年 Hirohito Kato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSLSynthesizer.
+FOUNDATION_EXPORT double KSLSynthesizerVersionNumber;
+
+//! Project version string for KSLSynthesizer.
+FOUNDATION_EXPORT const unsigned char KSLSynthesizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSLSynthesizer/PublicHeader.h>
+
+
---
+++ KSLSynthesizerTests/KSLSynthesizerTests.m
@@ -0,0 +1,39 @@
+//
+//  KSLSynthesizerTests.m
+//  KSLSynthesizerTests
+//
+//  Created by 加藤寛人 on 2016/01/08.
+//  Copyright © 2016年 Hirohito Kato. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KSLSynthesizerTests : XCTestCase
+
+@end
+
+@implementation KSLSynthesizerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
